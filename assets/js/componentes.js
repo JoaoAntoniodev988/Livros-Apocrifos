31c14368c8c6877a27d126f162ecb9e9b53f31f7
JoaoAntoniodev988 <joaodesousaantonio2@gmail.com>
2026-07-09 02:35:33 +0100

    d

--- assets/js/componentes.js
+++ assets/js/componentes.js
@@ -5,90 +5,48 @@
  * Carrega componentes HTML reutilizáveis.
  */
 
-
 async function loadComponent(selector, file) {
-
     const element = document.querySelector(selector);
-
-
     if (!element) {
         return;
     }
-
-
     try {
-
         const response = await fetch(file);
-
-
         if (!response.ok) {
-
             throw new Error(
                 `Erro ao carregar componente: ${file}`
             );
-
         }
-
-
         const html = await response.text();
-
-
         element.innerHTML = html;
-
-
     } catch(error) {
-
         console.error(error);
-
     }
 
 }
-
-
 
 /*
     Detecta o caminho base
 */
-
 function getBasePath() {
-
     const path = window.location.pathname;
-
-
     if (path.includes("/paginas/")) {
-
         return "../";
-
     }
-
-
     return "";
-
 }
-
-
 
 /*
     Carregamento dos componentes globais
 */
-
 document.addEventListener("DOMContentLoaded", () => {
-
-
     const basePath = getBasePath();
-
-
-
   loadComponent(
     "[data-component='header']",
     `${basePath}componentes/header.html`
 );
-
-
 loadComponent(
     "[data-component='footer']",
     `${basePath}componentes/footer.html`
 );
-
-
 });
